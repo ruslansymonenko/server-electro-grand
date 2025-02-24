49a94382990d1395b226903594742b10c1474cee
ruslansymonenko <symonenkors@gmail.com>
2025-02-24 17:57:28 +0200

    fixed error with building of production project

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:18
+FROM node:22.11.0
 
 WORKDIR /usr/src/app
 
--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -4,6 +4,11 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+      console.log('Database Connected!');
+    } catch (error) {
+      console.error('Error while connecting to database:', error);
+    }
   }
 }
